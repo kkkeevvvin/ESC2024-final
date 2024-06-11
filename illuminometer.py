296f2e8e1077e2da92e3204ae07598eb89050de6
kkkeevvvin <kevinliu.c@nycu.edu.tw>
2024-06-11 10:06:49 +0800

    illum test

--- illuminometer.py
+++ illuminometer.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|P|i|e|P|i|e|.|c|o|m|.|t|w|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#
-# camera_preview.py
-# Preview from camera
-#
-# Author : sosorry
-# Date   : 04/18/2015
-# Usage  : python3 picamera2_cv2_preview.py
 
 import cv2
 import time
 import numpy as np
 from picamera2 import Picamera2
 
-picam2 = Picamera2(0)
-# picam2.preview_configuration.main.size = (640, 480)
-# picam2.preview_configuration.main.format = "RGB888"
-picam2.start()
+class Illuminometer:
+    def __init__(self):
+        self.camera = Picamera2(0)
+        self.camera.start()
 
-# Start camera
-time.sleep(2)
+    def get(self):
+        frame = picam2.capture_array()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        average_intensity = np.mean(frame)
+        return average_intensity
 
-while True:
-    frame = picam2.capture_array()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("picamera2_cv2", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-    average_intensity = np.mean(frame)
-    print(f'Average Light Intensity: {average_intensity}')
-    time.sleep(1)
-
-# cv2.destroyAllWindows()
-
+if __name__ == "__main__":
+    x = Illuminometer()
+    print(x.get())
