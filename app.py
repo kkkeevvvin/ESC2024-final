6feda239f2b1534be78c9c8cd359414721d67075
kkkeevvvin <kevinliu.c@nycu.edu.tw>
2024-06-11 10:23:22 +0800

    illumi in app test

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template_string
 import RPi.GPIO as GPIO
 
-# Setup
-# GPIO.setmode(GPIO.BCM)
-# LED_PIN = 17
-# GPIO.setup(LED_PIN, GPIO.OUT)
-
 app = Flask(__name__)
+lightmeter = Illminometer()
 
 # HTML template
 html = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>LED Control</title>
+    <title>LED Control and Light Meter</title>
+    <script>
+        function getIlluminance() {
+            fetch('/illuminance')
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById('illuminance').innerText = data.illuminance;
+                });
+        }
+        setInterval(getIlluminance, 1000);
+        window.onload = getIlluminance;
+    </script>
 </head>
 <body>
     <h1>Control LED</h1>
@@ -23,6 +30,7 @@ html = """
     <form action="/off">
         <button type="submit">Turn OFF</button>
     </form>
+    <h2>Current Illuminance: <span id="illuminance">Loading...</span> lux</h2>
 </body>
 </html>
 """
@@ -42,6 +50,11 @@ def led_off():
     # GPIO.output(LED_PIN, GPIO.LOW)
     print("led off")
     return render_template_string(html)
+
+@app.route('/illuminance')
+def get_illuminance():
+    illuminance = lightmeter.get()
+    return jsonify({'illuminance': illuminance})
 
 if __name__ == '__main__':
     try:
