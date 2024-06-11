6466bb255a046cd37e9b32cdad2548c49615794d
kkkeevvvin <kevinliu.c@nycu.edu.tw>
2024-06-11 09:05:13 +0800

    temporarily disable led

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template_string
 import RPi.GPIO as GPIO
 
 # Setup
-GPIO.setmode(GPIO.BCM)
-LED_PIN = 17
-GPIO.setup(LED_PIN, GPIO.OUT)
+# GPIO.setmode(GPIO.BCM)
+# LED_PIN = 17
+# GPIO.setup(LED_PIN, GPIO.OUT)
 
 app = Flask(__name__)
 
@@ -33,12 +33,14 @@ def index():
 
 @app.route('/on')
 def led_on():
-    GPIO.output(LED_PIN, GPIO.HIGH)
+    # GPIO.output(LED_PIN, GPIO.HIGH)
+    print("led on")
     return render_template_string(html)
 
 @app.route('/off')
 def led_off():
-    GPIO.output(LED_PIN, GPIO.LOW)
+    # GPIO.output(LED_PIN, GPIO.LOW)
+    print("led off")
     return render_template_string(html)
 
 if __name__ == '__main__':
