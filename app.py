08ed524ae2f2f1285e80fa377c403b12b24497b3
kkkeevvvin <kevin900726@gmail.com>
2024-06-12 01:49:29 +0100

    seperate python and html

--- app.py
+++ app.py
@@ -1,56 +1,26 @@
-from flask import Flask, render_template_string, jsonify
+from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
 from illuminometer import Illuminometer
 
 app = Flask(__name__)
 lightmeter = Illuminometer()
-
-# HTML template
-html = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>LED Control and Light Meter</title>
-    <script>
-        function getIlluminance() {
-            fetch('/illuminance')
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('illuminance').innerText = data.illuminance;
-                });
-        }
-        setInterval(getIlluminance, 1000);
-        window.onload = getIlluminance;
-    </script>
-</head>
-<body>
-    <h1>Control LED</h1>
-    <form action="/on">
-        <button type="submit">Turn ON</button>
-    </form>
-    <form action="/off">
-        <button type="submit">Turn OFF</button>
-    </form>
-    <h2>Current Illuminance: <span id="illuminance">Loading...</span> lux</h2>
-</body>
-</html>
-"""
+html = "index.html"
 
 @app.route('/')
 def index():
-    return render_template_string(html)
+    return render_template(html)
 
 @app.route('/on')
 def led_on():
     # GPIO.output(LED_PIN, GPIO.HIGH)
     print("led on")
-    return render_template_string(html)
+    return render_template(html)
 
 @app.route('/off')
 def led_off():
     # GPIO.output(LED_PIN, GPIO.LOW)
     print("led off")
-    return render_template_string(html)
+    return render_template(html)
 
 @app.route('/illuminance')
 def get_illuminance():
